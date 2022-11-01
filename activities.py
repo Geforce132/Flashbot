46d635ef1757e5f131d2638f4895bb4146576a5f
Geforce <Geforce132@gmail.com>
2022-10-31 21:59:51 -0500

    Initial commit

---
+++ activities.py
@@ -0,0 +1,296 @@
+from uuid import uuid4
+from googleapiclient.discovery import build
+from requests import request
+import json
+import random
+import numpy as np
+import requests
+from math import floor
+from flashbot_secrets import SLIDES_MASTER_ID, MURAL_CLIENT, MURAL_SECRET, MURAL_ACCESS_TOKEN, MURAL_REFRESH_TOKEN, MURAL_WORKSPACE_ID, MURAL_ROOM_ID
+
+async def stoplight(ctx, creds, terms):
+    service = build('slides', 'v1', credentials=creds)
+
+    presentation = service.presentations().get(presentationId=SLIDES_MASTER_ID).execute()
+    slides = presentation.get('slides')
+    number_of_slides = len(slides)
+
+    new_slide_uuid = str(uuid4())
+    MAX_TERMS = 6
+
+    # Create a new square textbox, using the supplied element ID.
+    requests = [
+        {
+            'duplicateObject': {
+                'objectId': ACTIVITY_INFO['stoplight']['slide_id'],
+                'objectIds': {
+                    ACTIVITY_INFO['stoplight']['slide_id']: new_slide_uuid
+                }
+            },
+        },
+        {
+            "replaceAllText": {
+                "replaceText": ACTIVITY_INFO['stoplight']['name'],
+                "pageObjectIds": [new_slide_uuid],
+                "containsText": {
+                    "text": "Template slide - " + ACTIVITY_INFO['stoplight']['name'],
+                    "matchCase": False
+                }
+            },
+        }
+    ]
+
+    for i in range(MAX_TERMS):
+        requests.append({
+            "replaceAllText": {
+                "replaceText": terms[i] if 0 <= i < len(terms) else " ",
+                "pageObjectIds": [new_slide_uuid],
+                "containsText": {
+                    "text": "Topic " + str(i + 1),
+                    "matchCase": True
+                }
+            },
+        })
+
+    requests.append({
+        "updateSlidesPosition": {
+            "slideObjectIds": [new_slide_uuid],
+            "insertionIndex": number_of_slides + 1
+        }
+    })
+
+    # Execute the request.
+    body = {
+        'requests': requests
+    }
+
+    response = service.presentations().batchUpdate(presentationId=SLIDES_MASTER_ID, body=body).execute()
+    new_slide_id = response['replies'][0]['duplicateObject']['objectId']
+
+    await ctx.send('A Stop Light slide has been created! Copy it from here into your own presentation: <https://docs.google.com/presentation/d/' + SLIDES_MASTER_ID + '/view#slide=id.' + new_slide_id + ">")
+
+async def catastrophe(ctx, mural_name, terms):
+    baseURL = "https://app.mural.co"
+    authBaseURL = baseURL + "/api/public/v1/authorization/oauth2"
+    tokenURL = baseURL + "/api/public/v1/authorization/oauth2/token"
+    scope= "murals:read murals:write"
+    
+    #mural = OAuth2Session(MURAL_CLIENT, scope=scope)
+    #authURL, state = mural.authorization_url(authBaseURL)
+    #print("Auth:", authURL)
+
+    #request = requests.post('https://app.mural.co/api/public/v1/authorization/oauth2/token', headers={'Content-Type': 'application/x-www-form-urlencoded'}, data={'client_id': MURAL_CLIENT, 'client_secret': MURAL_SECRET, 'redirect_uri': 'http://localhost:8005', 'code': 'account_code', 'grant_type': 'authorization_code'})
+    #print("Code:", request)
+    #print(request.json())
+    request = requests.post('https://app.mural.co/api/public/v1/authorization/oauth2/token', headers={'Content-Type': 'application/x-www-form-urlencoded'}, data={'client_id': MURAL_CLIENT, 'client_secret': MURAL_SECRET, 'redirect_uri': 'http://localhost:8005', 'refresh_token': MURAL_REFRESH_TOKEN, 'grant_type': 'refresh_token'})
+    
+    if request.status_code == 200:
+        access_token = request.json()['access_token']
+        mural_id = mural_create(access_token, MURAL_WORKSPACE_ID, MURAL_ROOM_ID, mural_name)
+        
+        width = (floor(len(terms) / 10) * 600) + 900
+        height = (floor(len(terms) / 10) * 400) + 700
+
+        placeAllStickies(mural_id, access_token, {'x': 50, 'y': 50, 'width': width, 'height': height}, 150, 150, terms)
+        mural_add_shape(mural_id, access_token, 'square', 50, 50, width, height)
+        await ctx.send("Here's a Catastrophe mural for you! Facilitator link (for SIL, requires login): <" + mural_get_member_link(mural_id, access_token) + ">\nGuest link (for students or SIL): <" + mural_get_visitor_link(mural_id, access_token) + ">")
+
+
+def mural_create( auth_token, workspace_id, room_id, title ):
+    # https://developers.mural.co/public/reference/createmural
+    url = "https://app.mural.co/api/public/v1/murals"
+    headers = { "Accept": "application/json", "Content-Type": "application/json", "Authorization": "Bearer " + auth_token }
+    parms = { "workspaceId": workspace_id, "roomId": int(room_id), "title": title }
+    response = request("POST", url, headers = headers, json = parms)
+    response_json = json.loads( response.text )
+    
+    msg = ''
+    if 'code' in response_json:
+        msg += response_json['code'] + ' '
+    if 'message' in response_json:
+        msg += response_json['message']
+    if msg != '':
+        print( msg )
+        return None
+    if 'value' not in response_json:
+        print('No value returned')
+        return None
+    if 'id' not in response_json['value']:
+        print('No id returned')
+        return None
+    return response_json['value']['id']
+
+def mural_add_sticky( mural_id, auth_token, sticky_data ):
+    # https://developers.mural.co/public/reference/createstickynote
+    url = "https://app.mural.co/api/public/v1/murals/" + mural_id + "/widgets/sticky-note"
+    headers = { "Content-Type" : "application/json", "Accept" : "vnd.mural.preview", "Authorization" : "Bearer " + auth_token }
+    data = json.dumps(sticky_data)
+    response = request("POST", url, headers=headers, data=data)
+    response_json = json.loads(response.text)
+    
+    msg = ""
+    if "code" in response_json:
+        msg += response_json["code"] + " "
+    if "message" in response_json:
+        msg += response_json["message"]
+    if msg != "":
+        print(msg)
+
+def mural_add_shape(mural_id, auth_token, shape: str, x, y, width=10, height=10):
+    url = "https://app.mural.co/api/public/v1/murals/" + mural_id + "/widgets/shape"
+    headers = { "Content-Type" : "application/json", "Accept" : "vnd.mural.preview", "Authorization" : "Bearer " + auth_token }
+
+    data = json.dumps({'shape': shape, 'x': x, 'y': y, 'width': width, 'height': height})
+    response = request("POST", url, headers=headers, data=data)
+    response_json = json.loads(response.text)
+    
+    msg = ""
+    if "code" in response_json:
+        msg += response_json["code"] + " "
+    if "message" in response_json:
+        msg += response_json["message"]
+    if msg != "":
+        print(msg)
+
+def mural_get_member_link(mural_id, auth_token):
+    url = "https://app.mural.co/api/public/v1/murals/" + mural_id
+    headers = { "Content-Type" : "application/json", "Accept" : "vnd.mural.preview", "Authorization" : "Bearer " + auth_token }
+
+    response = request("GET", url, headers=headers)
+    response_json = json.loads(response.text)
+    
+    msg = ""
+    if "code" in response_json:
+        msg += response_json["code"] + " "
+    if "message" in response_json:
+        msg += response_json["message"]
+    if msg != "":
+        print(msg)
+
+    return response_json['value']['sharingSettings']['link']
+
+def mural_get_visitor_link(mural_id, auth_token, visitor_perm="write", member_perm="write"):
+    url = "https://app.mural.co/api/public/v1/murals/" + mural_id + "/visitor-settings"
+    headers = { "Content-Type" : "application/json", "Accept" : "vnd.mural.preview", "Authorization" : "Bearer " + auth_token }
+
+    data = json.dumps({'visitors': visitor_perm, 'workspaceMembers': member_perm})
+    response = request("PATCH", url, headers=headers, data=data)
+    print("***", response, response.status_code)
+    response_json = json.loads(response.text)
+    
+    msg = ""
+    if "code" in response_json:
+        msg += response_json["code"] + " "
+    if "message" in response_json:
+        msg += response_json["message"]
+    if msg != "":
+        print(msg)
+
+    if 'value' in response_json and 'link' in response_json['value']:
+        return response_json['value']['link']
+    else:
+        return "unknown error when getting visitor link: " + response.status_code
+
+def placeAllStickies( mural_id, auth_token, rectangle, sticky_width, sticky_height, text_arr ):
+    attempt = 0
+
+    while attempt < 3:
+        coords = makeRandomCoords( rectangle, sticky_width, sticky_height )
+        stickies_arr = []
+        for txt in text_arr:
+            try:
+                x, y = coords.pop()
+                count = 0
+                while ( count <= 3 ) and overlaps( x, y, sticky_width, sticky_height, stickies_arr ):
+                    x, y = coords.pop()
+                    count += 1
+                sticky = { "x"      : x, 
+                        "y"      : y, 
+                        "width"  : sticky_width, 
+                        "height" : sticky_height, 
+                        "shape"  : "rectangle", 
+                        "style"  : { "backgroundColor" : "#b5e5cfFF", 'fontSize': 18 }, 
+                        "text"   : txt }
+                mural_add_sticky( mural_id, auth_token, sticky )
+                stickies_arr.append( sticky )
+            except IndexError:
+                attempt += 1
+        
+        if len(text_arr) == len(stickies_arr):
+            break
+
+        attempt += 1
+
+def makeRandomCoords(rectangle, sticky_width, sticky_height):
+    x_start = round( rectangle["x"] + ( 0.5 * sticky_width ) )
+    x_end   = round( rectangle["x"] + rectangle["width"] - ( 1.5 * sticky_width ) )
+    y_start = round( rectangle["y"] + ( 0.5 * sticky_height ) )
+    y_end   = round( rectangle["y"] + rectangle["height"] - ( 1.5 * sticky_height ) )
+    x_list = np.arange( x_start, x_end, round( 0.6 * sticky_width ) ).tolist()
+    x_list += x_list
+    random.shuffle( x_list )
+    random.shuffle( x_list )
+    y_list = np.arange( y_start, y_end, round( 0.6 * sticky_width ) ).tolist()
+    y_list += y_list
+    random.shuffle( y_list )
+    random.shuffle( y_list )
+    x_length = len( x_list )
+    y_length = len( y_list )
+    list_length = x_length if ( x_length < y_length ) else y_length
+    coords = []
+    for i in range( list_length ):
+        x = x_list[i] + 10 * random.random()
+        y = y_list[i] + 10 * random.random()
+        coords.append( [ x, y ] )
+    return coords
+
+def stickyInsideOverlapsRectangle(sticky, rectangle):
+    #
+    left_edge_overlaps = False
+    if ( sticky["x"] >= rectangle["x"] ) and \
+       ( sticky["x"] <= ( rectangle["x"] + rectangle["width"] ) ):
+        left_edge_overlaps = True
+    #
+    right_edge_overlaps = False
+    if ( ( sticky["x"] + sticky["width"] ) >= rectangle["x"] ) and \
+       ( ( sticky["x"] + sticky["width"] ) <= ( rectangle["x"] + rectangle["width"] ) ):
+        right_edge_overlaps = True
+    #
+    top_edge_overlaps = False
+    if ( sticky["y"] >= rectangle["y"] ) and \
+       ( sticky["y"] <= ( rectangle["y"] + rectangle["height"] ) ):
+        top_edge_overlaps = True
+    #
+    bottom_edge_overlaps = False
+    if ( ( sticky["y"] + sticky["height"] ) >= rectangle["y"] ) and \
+       ( ( sticky["y"] + sticky["height"] ) <= ( rectangle["y"] + rectangle["height"] ) ):
+        bottom_edge_overlaps = True
+    
+    if left_edge_overlaps and right_edge_overlaps and top_edge_overlaps and bottom_edge_overlaps:
+        return "inside"
+    
+    if ( left_edge_overlaps or right_edge_overlaps ) and ( top_edge_overlaps or bottom_edge_overlaps ):
+        return "overlaps"
+    
+    return "outside"
+
+def overlaps(x, y, sticky_width, sticky_height, stickies_arr):
+    sticky_in = { "x" : x, "y" : y, "width" : sticky_width, "height" : sticky_height }
+    for existing_sticky in stickies_arr:
+        position = stickyInsideOverlapsRectangle( sticky_in, existing_sticky )
+        if ( "overlaps" == position ) or ( "inside" == position ):
+            return True
+    return False
+
+ACTIVITY_INFO = {'stoplight': {
+                  'name': 'Stop Light',
+                  'function': stoplight,
+                  'syntax': 'To create a Stop Light slide, use the following syntax: !create stoplight [terms separated by commas (min 1, max 6)]',
+                  'slide_id': 'g179cef56cbb_0_0',
+                 },
+                 'catastrophe': {
+                  'name': 'Catastrophe',
+                  'function': catastrophe,
+                  'syntax': "To create a Catastrophe mural, use the following syntax: !create catastrophe [terms separated by commas (min 2). The first term will be used as the mural's name]"
+                 },
+                }
---
+++ main.py
@@ -0,0 +1,274 @@
+import discord
+from discord.ext import commands
+from json import loads
+from random import randrange
+import os.path
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+from activities import ACTIVITY_INFO
+from flashbot_secrets import DISCORD_SECRET
+#from requests_oauthlib import OAuth2Session
+
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix='!', intents=intents)
+
+DEFAULT_SESSION_TIME_MINUTES = 60
+
+ACTIVITY_TYPE_EITHER = 0
+ACTIVITY_TYPE_OPENER = 1
+ACTIVITY_TYPE_MAIN = 2
+
+SCOPES = ['https://www.googleapis.com/auth/presentations']
+
+@client.group()
+async def create(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send("Known activities are: [" + ' | '.join(list(ACTIVITY_INFO.keys())) + "]")
+
+@create.command()
+async def stoplight(ctx, *, message: str=""):
+    if message != "":
+        creds = None
+        
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+
+        try:
+            terms = message.split(",")
+
+            if len(terms) > 6:
+                await ctx.send(ACTIVITY_INFO['stoplight']['syntax'])
+                return
+
+            await ACTIVITY_INFO['stoplight']['function'](ctx, creds, terms=terms)
+        except HttpError as error:
+            await ctx.send(f'An error occurred: {error}')
+    else:
+        await ctx.send(ACTIVITY_INFO['stoplight']['syntax'])
+
+@create.command()
+async def catastrophe(ctx, *, message: str=""):
+    if message != "":
+        info = message.split(",", 1)
+
+        if len(info) == 2:
+            mural_name = info[0]
+            info[1] = info[1].strip()
+            terms = info[1].split(',')
+
+            await ACTIVITY_INFO['catastrophe']['function'](ctx, mural_name, terms)
+        else:
+            await ctx.send(ACTIVITY_INFO['catastrophe']['syntax'])
+    else:
+        await ctx.send(ACTIVITY_INFO['catastrophe']['syntax'])
+
+@client.command()
+async def sessionplan(ctx, *args):
+    num_args = len(args)
+
+    if num_args == 0:
+        await create_plan(ctx)
+    elif len(args) == 1:
+        try:
+            mins = int(args[0])
+            await create_plan(ctx, minutes=mins)
+        except:
+            await ctx.send('Non-numbers are not allowed')
+
+@client.command()
+async def activity(ctx, *args):
+    if len(args) > 0:
+        name = (' '.join(args)).lower()
+
+        for i in range(num_strategies):
+            if strategies[i]['name'].lower() == name:
+                await ctx.send(strategies[i]['name'] + ' description: ' + strategies[i]['description'] + '\nVARK: ' + strategies[i]['vark'])
+                return
+        
+        await ctx.send(name + ' not found!')
+
+@client.command()
+async def schedule(ctx, *, message: str = ""):
+    if message != "":
+        campus = message.lower()
+        campus_codes = {"northeast": "NE", "ne": "NE", "northwest": "NW", "nw": "NW", "south": "South", "so": "South", "southeast": "SE", "se": "SE", "trinity": "TR", "trinity river": "TR", "tr": "TR", "connect": "CN", "cn": "CN"}
+        
+        if campus in campus_codes.keys():
+            await ctx.send("TCC " + campus_codes[campus] + " SI schedule: <https://tinyurl.com/TCC" + campus_codes[campus] + "SISchedule>")
+        else:
+            await ctx.send("Campus '" + campus + "' not found. Perhaps you have a typo?")
+    else:
+        await ctx.send("Please include a campus name or abbreviation")
+
+async def create_plan(ctx, minutes=DEFAULT_SESSION_TIME_MINUTES):
+    good_plan = False
+    
+    while not good_plan:
+        activity_types = [ACTIVITY_TYPE_OPENER, ACTIVITY_TYPE_MAIN, ACTIVITY_TYPE_MAIN, ACTIVITY_TYPE_EITHER, ACTIVITY_TYPE_OPENER]
+        times = pick_time_layout(minutes)
+        sel_strats = []
+        vark = ''
+
+        for i in range(5):
+            strat = pick_activity(sel_strategies=sel_strats, activity_type=activity_types[i])
+            vark += strat['vark']
+            sel_strats.append(strat)
+        
+        if check_plan(sel_strats, vark, times):
+            good_plan = True
+
+    embed = discord.Embed(title="Here's a session plan for you!")
+
+    activity_names = ['Opener', 'Main activity 1', 'Main activity 2', 'Backpocket activity', 'Closer']
+    for index, strat in enumerate(sel_strats):
+        embed.add_field(name=activity_names[index], value=f"{strat['name']} {' '.join(get_activity_emoji_string(strat))}\nTime: {times[index]} - VARK: {strat['vark']}", inline=False)
+
+    await ctx.send(embed=embed)
+
+def check_plan(strategies, vark, times):
+    good_plan = False
+
+    has_all_vark = 'V' in vark and 'A' in vark and 'R' in vark and 'K' in vark
+    if has_all_vark:
+        vark_times = dict(V=0, A=0, R=0, K=0)
+
+        for i, strat in enumerate(strategies):
+            time_to_add = times[i] if times[i] != 'filler' else 0
+            
+            if 'V' in strat['vark']:
+                vark_times['V'] += time_to_add
+
+            if 'A' in strat['vark']:
+                vark_times['A'] += time_to_add
+
+            if 'R' in strat['vark']:
+                vark_times['R'] += time_to_add
+
+            if 'K' in strat['vark']:
+                vark_times['K'] += time_to_add
+        
+        if all(value > 15 for value in vark_times.values()):
+            good_plan = True
+
+    return good_plan
+
+def pick_activity(sel_strategies=[], activity_type=ACTIVITY_TYPE_MAIN):
+    good_act = False
+    iterations = 0
+
+    while not good_act:
+        iterations += 1
+        act = strategies[randrange(1, num_strategies)]
+
+        # already selected
+        if act in sel_strategies:
+            continue
+
+        # not the correct type of activity
+        if not ACTIVITY_TYPE_EITHER:
+            if (act['mainActivity'] == 'true' and activity_type == ACTIVITY_TYPE_OPENER) or (act['mainActivity'] == 'false' and activity_type == ACTIVITY_TYPE_MAIN):
+                continue
+
+        # same VARK designations as the previous activity
+        if len(sel_strategies) >= 1 and sel_strategies[-1]['vark'] == act['vark']:
+            continue
+
+        # too much of this VARK type of activity already selected
+        if iterations <= 100:
+            excessive_vark = False
+            counts = get_strategy_vark_counts(sel_strategies)
+            for char in 'VARK':
+                if counts[char] >= 3 and char in act['vark']:
+                    excessive_vark = True
+        
+            if excessive_vark:
+                continue
+
+        good_act = True
+
+    return act
+
+def get_activity_emoji_string(activity):
+    vark = activity['vark']
+    str = ''
+
+    if 'V' in vark:
+        str += '👀'
+    
+    if 'A' in vark:
+        str += '🗣️'
+    
+    if 'R' in vark:
+        str += '📝'
+
+    if 'K' in vark:
+        str += '🏃'
+
+    return str
+
+def get_strategy_vark_counts(strats):
+    counts = dict(V=0, A=0, R=0, K=0)
+
+    for i in strats:
+        if 'V' in i['vark']:
+            counts['V'] += 1
+    
+        if 'A' in i['vark']:
+            counts['A'] += 1
+    
+        if 'R' in i['vark']:
+            counts['R'] += 1
+
+        if 'K' in i['vark']:
+            counts['K'] += 1
+    
+    return counts
+
+def pick_time_layout(minutes=DEFAULT_SESSION_TIME_MINUTES):
+    times = [
+        [10, 20, 20, "filler", 10],
+        [10, 20, 20, "filler", 10],
+        [10, 20, 20, "filler", 10],
+        [10, 20, 20, "filler", 10],
+        [10, 15, 25, "filler", 10],
+        [15, 20, 20, "filler", 5]
+    ]
+
+    short_times = [
+        [10, 15, 20, "filler", 5],
+        [10, 15, 20, "filler", 5],
+        [10, 15, 20, "filler", 5],
+        [5, 20, 20, "filler", 5],
+        [10, 15, 15, "filler", 10]
+    ]
+
+    if minutes == 50:
+        return short_times[randrange(0, len(short_times))]
+    else:
+        return times[randrange(0, len(times))]
+
+if __name__ == "__main__":
+    with open('cards.json', 'r', encoding='utf-8') as file:
+        strategies = loads(file.read())['strategies']
+        num_strategies = len(strategies)
+
+    # Last line
+    client.run(DISCORD_SECRET)
+
+
